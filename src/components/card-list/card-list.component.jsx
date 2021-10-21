07ea2f9b156bb8802654d395c866cee551e51689
David Buchanan <dbuchana@verizon.net>
2021-10-21 11:57:54 -0400

    Latest monster changes as of lesson 48

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Card } from '../card/card.component';
+
+import './card-list.styles.css';
+
+export const CardList = (props) => {
+    //console.log("props.monsters=", props.monsters );
+    //let monsterNames = props.monsters.map( (monster) => monster.name);
+    //console.log("monsterNames=", monsterNames);
+    return (
+        <div className='card-list'>
+            {props.monsters.map( monster => (
+                <Card key={monster.id} monster={monster} />
+            ))}
+        </div>
+    )
+}
